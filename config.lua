96de38e15183d688e16fb5f48dfa81d30d91af49
Faisal Otaibi <65165121+FaisalAyed@users.noreply.github.com>
2023-10-26 13:07:06 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+Config.Object = {
+    [1] = {
+        coord = vector3(610.5770, -428.5407, 24.7400),
+        heading = -185.0,
+        object = "prop_tool_bench02",
+    },
+    [2] = {
+        coord = vector3(599.7758, -393.3946, 24.7471 ),
+        heading = 283.4771,
+        object = "prop_fan_01",
+    },
+    [3] = {
+        coord = vector3(588.2643, -395.4632, 24.7296 ),
+        heading = 108.0096,
+        object = "prop_pile_dirt_04",
+    }
+}
+
+Config.Peds = {
+    [1] = {
+        coord = vector3(610.4570, -430.8776, 24.7400),
+        heading =  89.0734,
+        hash = "mp_m_waremech_01",
+        animition = "WORLD_HUMAN_BINOCULARS",
+    },
+}
